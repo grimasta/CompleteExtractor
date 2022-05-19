da150c1c83bfecc867ebb51e49c0d16ebee06e80
MariosGrigoriou <marios.grigoriou@ibm.com>
2022-05-18 20:24:50 -0400

    Fixed unchecked creation of scripts in main extraction folder
    there were 2 stray file creations in the method
    run(String, String) in SshConsole
    one was deleted and the other taken care off after execution is
    completed

--- src/console/commanders/SshConsole.java
+++ src/console/commanders/SshConsole.java
@@ -29,77 +29,9 @@ public class SshConsole implements Console {
 	// "bash --login -c 'env'"
 //	ssh -F C:\Users\spawn\vagrant_workspaces\ExtractorSpace\vagrant-ssh default "/vagrant/run_extractor.sh"
 
-//	public int run(String command, String target) {
-//		int retries = 4;
-//		int currentTry = 0;
-//		Runtime thisRuntime = Runtime.getRuntime();
-//		try {
-////			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("../../run_script.sh")));
-////			bw.write(command);
-////			bw.close();
-////			System.out.println("command being run:" + command);
-//			String bashScriptPath = "../../bash_script" + target + ".bat"; 
-//			File bashScript = new File(bashScriptPath);
-//			BufferedWriter bashScriptWriter = new BufferedWriter(new FileWriter(bashScript));
-//			bashScriptWriter.write("ssh -F " + RunConfiguration.getVagrantSshScriptLocation() + " default \"bash --login -c '" + command + "'\"");
-//			bashScriptWriter.close();
-//			bashScript.delete();
-//			
-//			Process executionProcess = thisRuntime.
-//					exec("cmd /c " + "..\\\\..\\\\bash_script" + target + ".bat");
-//			executionProcess.waitFor();
-//			currentTry++;
-//			while (executionProcess.exitValue() != 0) {
-//				if (currentTry < retries) {
-//					try {
-//						Thread.sleep(1000*currentTry);
-//					} catch (InterruptedException ie) {
-//						System.out.println(ie.getMessage());
-//					}
-//					currentTry ++;
-//					executionProcess = thisRuntime.
-//							exec("cmd /c " + "..\\\\..\\\\bash_script" + target + ".bat");
-//					executionProcess.waitFor();
-//				} else
-//					break;
-//			}
-////			BufferedReader br = new BufferedReader(new InputStreamReader(executionProcess.getInputStream()));
-////			String s;
-////			while ((s = br.readLine()) != null)
-////				System.out.println("Output : " + s);
-//			if (executionProcess.exitValue() != 0) {
-//				System.out.println("Command : " + command + " ||| exited with code : " + executionProcess.exitValue() + target);
-//				BufferedReader br = new BufferedReader(new InputStreamReader(executionProcess.getInputStream()));
-//				String s;
-//				while ((s = br.readLine()) != null)
-//					System.out.println("Output : " + s);
-//				return executionProcess.exitValue();	
-//			}
-//			bashScript.delete();
-//			return executionProcess.exitValue();
-//		} catch (IOException ioe) {
-//			System.out.println("exception raised when executing command : ");
-//			System.out.println(command);
-//			System.out.println("in SshConsole");
-//			System.out.println("IOException message = " + ioe.getMessage());
-//			return -1;
-//		} catch (InterruptedException ie) {
-//			System.out.println("exception raised when executing command : ");
-//			System.out.println(command);
-//			System.out.println("in SshConsole");
-//			System.out.println("InterruptedException message = " + ie.getMessage());
-//			return -1;
-//		}
-//
-//	}
-
 	public int run(String command, String target) {
 		Runtime thisRuntime = Runtime.getRuntime();
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("../../run_script" + target + ".sh")));
-			bw.write(command);
-//			System.out.println("command being run:" + command);
-			bw.close();
 			String bashScriptPath = "../../bash_script" + target + ".bat"; 
 			File bashScript = new File(bashScriptPath);
 			BufferedWriter bashScriptWriter = new BufferedWriter(new FileWriter(bashScript));
@@ -117,6 +49,7 @@ public class SshConsole implements Console {
 				while ((s = br.readLine()) != null)
 					System.out.println("Output : " + s);
 			}
+			bashScript.delete();
 			return executionProcess.exitValue();
 		} catch (IOException ioe) {
 			System.out.println("exception raised when executing command : ");
@@ -134,7 +67,7 @@ public class SshConsole implements Console {
 
 	}
 
-	
+//	unused
 	public int run(String command, String[] envp, File dir, String target) {
 		Runtime thisRuntime = Runtime.getRuntime();
 		try {
