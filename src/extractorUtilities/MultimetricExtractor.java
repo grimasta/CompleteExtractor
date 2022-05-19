6910eac155048af821b6e1dcb8c1fa4f122134e5
mgrigori <mgrigori@PCE-17.uwo.ca>
2022-05-18 22:04:01 -0400

    Reverted to Ignoring paths with spaces,
    cannot find a way around it yet

--- src/extractorUtilities/MultimetricExtractor.java
+++ src/extractorUtilities/MultimetricExtractor.java
@@ -77,7 +77,8 @@ public class MultimetricExtractor implements ExtractionMethod {
 						line.toLowerCase().endsWith(".rb") || line.toLowerCase().endsWith(".js") ||
 						line.toLowerCase().endsWith(".go") || line.toLowerCase().endsWith(".sh")) {
 					if (line.toLowerCase().contains(" "))
-						line = line.replaceAll("\\s+", "\\\\ ");
+						continue;
+//						line = line.replaceAll("\\s+", "\\ ");
 					//				filter out all files that are not c or cpp source or header files as well java source files
 					multimetricInput += " " + line;
 				}
